d4321a87b9a91332bd41164b410dbc8693c2a6f5
tuheli <elias.tuhola@gmail.com>
2023-08-24 12:13:14 +0300

    Create RepositoryItem.jsx

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  console.log('repo item props', item);
+  const {
+    fullName,
+    description,
+    language,
+    stargazersCount,
+    forksCount,
+    reviewCount,
+    ratingAverage,
+  } = item;
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
