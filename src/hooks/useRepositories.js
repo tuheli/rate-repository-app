1c485fe8394044798e21eb573453adaa10e9efa9
tuheli <elias.tuhola@gmail.com>
2023-08-29 08:46:54 +0300

    Create useRepositories.js

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const response = await fetch('http://172.20.10.2:5000/api/repositories');
+    const json = await response.json();
+
+    console.log('response json', json);
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
