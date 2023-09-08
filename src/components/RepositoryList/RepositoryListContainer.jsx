a1f159b0226a24e271ee343beb386fef22238cda
tuheli <elias.tuhola@gmail.com>
2023-09-08 15:33:30 +0300

    Moved ItemSeparator to separate file

--- src/components/RepositoryList/RepositoryListContainer.jsx
+++ src/components/RepositoryList/RepositoryListContainer.jsx
@@ -1,15 +1,8 @@
-import { FlatList, View, StyleSheet, Pressable } from 'react-native';
+import { FlatList, Pressable } from 'react-native';
 import { useNavigate } from 'react-router-native';
 
 import RepositoryItem from './RepositoryItem';
-
-const styles = StyleSheet.create({
-  separator: {
-    height: 10,
-  },
-});
-
-const ItemSeparator = () => <View style={styles.separator} />;
+import ItemSeparator from './ItemSeparator';
 
 const RepositoryListContainer = ({ repositories }) => {
   const navigate = useNavigate();
