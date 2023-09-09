f3cc094a12e2e7b8b47bb1447a70c11520100db0
tuheli <elias.tuhola@gmail.com>
2023-09-09 20:47:05 +0300

    Added sorting options

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,10 +2,14 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 import { useState } from 'react';
 
-const useRepositories = () => {
+const useRepositories = (orderBy = 'CREATED_AT', orderDirection = 'DESC') => {
   const [repositories, setRepositories] = useState(undefined);
   const { loading, refetch } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
+    variables: {
+      orderBy,
+      orderDirection,
+    },
     onCompleted: (data) => {
       console.log('Query completed: GET_REPOSITORIES');
       setRepositories(data.repositories);
