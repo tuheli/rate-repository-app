80324891165ccbe12eaa37c33200f9601233768e
tuheli <elias.tuhola@gmail.com>
2023-09-09 21:39:58 +0300

    Update index.jsx

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Button } from 'react-native';
 
 import useRepositories from '../../hooks/useRepositories';
 import RepositoryListContainer from './RepositoryListContainer';
@@ -13,25 +12,11 @@ const RepositoryList = () => {
   console.log('repositories at repo list', repositories);
 
   return (
-    <>
-      <Button
-        title="Order by rating"
-        onPress={() => setOrderBy('RATING_AVERAGE')}
-      />
-      <Button
-        title="Order by created at"
-        onPress={() => setOrderBy('CREATED_AT')}
-      />
-      <Button
-        title="Order direction desc"
-        onPress={() => setOrderDirection('DESC')}
-      />
-      <Button
-        title="Order direction asc"
-        onPress={() => setOrderDirection('ASC')}
-      />
-      <RepositoryListContainer repositories={repositories} />
-    </>
+    <RepositoryListContainer
+      repositories={repositories}
+      setOrderBy={setOrderBy}
+      setOrderDirection={setOrderDirection}
+    />
   );
 };
 
