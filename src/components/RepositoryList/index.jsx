079dc49e581203352db6d1a452a4323bee85795c
tuheli <elias.tuhola@gmail.com>
2023-09-09 20:47:43 +0300

    Added testing buttons for ordering repositories

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,12 +1,38 @@
+import { useState } from 'react';
+import { Button } from 'react-native';
+
 import useRepositories from '../../hooks/useRepositories';
 import RepositoryListContainer from './RepositoryListContainer';
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [orderBy, setOrderBy] = useState('CREATED_AT');
+  const [orderDirection, setOrderDirection] = useState('DESC');
+
+  const { repositories } = useRepositories(orderBy, orderDirection);
 
   console.log('repositories at repo list', repositories);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <>
+      <Button
+        title="Order by rating"
+        onPress={() => setOrderBy('RATING_AVERAGE')}
+      />
+      <Button
+        title="Order by created at"
+        onPress={() => setOrderBy('CREATED_AT')}
+      />
+      <Button
+        title="Order direction desc"
+        onPress={() => setOrderDirection('DESC')}
+      />
+      <Button
+        title="Order direction asc"
+        onPress={() => setOrderDirection('ASC')}
+      />
+      <RepositoryListContainer repositories={repositories} />
+    </>
+  );
 };
 
 export default RepositoryList;
