beafee537348d313338282c3e0848da9322280a0
tuheli <elias.tuhola@gmail.com>
2023-09-10 11:45:57 +0300

    Updated query to include search keyword

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,13 +2,19 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 import { useState } from 'react';
 
-const useRepositories = (orderBy = 'CREATED_AT', orderDirection = 'DESC') => {
+const useRepositories = (
+  orderBy = 'CREATED_AT',
+  orderDirection = 'DESC',
+  searchKeyword = ''
+) => {
   const [repositories, setRepositories] = useState(undefined);
+
   const { loading, refetch } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     variables: {
       orderBy,
       orderDirection,
+      searchKeyword,
     },
     onCompleted: (data) => {
       console.log('Query completed: GET_REPOSITORIES');
