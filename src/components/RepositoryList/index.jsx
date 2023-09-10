e9529058cbfc289b35175bb41cacf26350ef30c2
tuheli <elias.tuhola@gmail.com>
2023-09-10 11:44:23 +0300

    Added search keyword with debounce

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,21 +1,32 @@
 import { useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
 
 import useRepositories from '../../hooks/useRepositories';
 import RepositoryListContainer from './RepositoryListContainer';
 
+const setKeywordDelay = 500;
+
 const RepositoryList = () => {
   const [orderBy, setOrderBy] = useState('CREATED_AT');
   const [orderDirection, setOrderDirection] = useState('DESC');
+  const [searchKeyword, setSearchKeyword] = useState('');
 
-  const { repositories } = useRepositories(orderBy, orderDirection);
+  const { repositories } = useRepositories(
+    orderBy,
+    orderDirection,
+    searchKeyword
+  );
 
-  console.log('repositories at repo list', repositories);
+  const debouncedSetSearchKeyword = useDebouncedCallback((value) => {
+    setSearchKeyword(value);
+  }, setKeywordDelay);
 
   return (
     <RepositoryListContainer
       repositories={repositories}
       setOrderBy={setOrderBy}
       setOrderDirection={setOrderDirection}
+      setSearchKeyword={debouncedSetSearchKeyword}
     />
   );
 };
