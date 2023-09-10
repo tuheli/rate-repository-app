bc6aacafe833d8cee4137ad9e8025f98a2745265
tuheli <elias.tuhola@gmail.com>
2023-09-10 21:34:16 +0300

    Update useMe.js

--- src/hooks/useMe.js
+++ src/hooks/useMe.js
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client';
 import { ME } from '../graphql/queries';
 
 const useMe = (includeReviews = false) => {
-  const { data } = useQuery(ME, {
+  const { data, refetch } = useQuery(ME, {
     fetchPolicy: 'no-cache',
     variables: {
       includeReviews,
@@ -11,7 +11,7 @@ const useMe = (includeReviews = false) => {
 
   const me = data ? data.me : { me: null };
 
-  return me;
+  return [me, refetch];
 };
 
 export default useMe;
