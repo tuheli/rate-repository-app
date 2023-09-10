0b1f92f8115f407821b79c9090ebf41ad3b6cefa
tuheli <elias.tuhola@gmail.com>
2023-09-10 21:36:53 +0300

    Added delete review function + small refactor

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -1,19 +1,37 @@
 import { FlatList } from 'react-native';
 import { Pressable } from 'react-native';
+import { useMutation } from '@apollo/client';
 
 import useMe from '../hooks/useMe';
 import ItemSeparator from './RepositoryList/ItemSeparator';
 import ReviewItem from './RepositoryList/RepositoryItem/ReviewItem';
+import { DELETE_REVIEW } from '../graphql/mutations';
 
 const MyReviews = () => {
-  const { reviews } = useMe(true);
+  const [me, refetch] = useMe(true);
+  const [mutate] = useMutation(DELETE_REVIEW);
 
-  const reviewNodes = reviews ? reviews.edges.map((edge) => edge.node) : [];
+  const reviewNodes =
+    me && me.reviews ? me.reviews.edges.map((edge) => edge.node) : [];
 
   console.log('review nodes', reviewNodes);
 
   const handleReviewClick = () => {
     console.log('handle my review click');
+  };
+
+  const handleDeleteClick = async (reviewId) => {
+    console.log('at handle delete review for', reviewId);
+
+    const { data } = await mutate({
+      variables: {
+        deleteReviewId: reviewId,
+      },
+    });
+
+    refetch();
+
+    console.log('review delete ok?', data);
   };
 
   return (
@@ -22,7 +40,12 @@ const MyReviews = () => {
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => (
         <Pressable onPress={() => handleReviewClick()}>
-          <ReviewItem review={item} />
+          <ReviewItem
+            review={item}
+            showButtons={true}
+            titleAsUsername={false}
+            handleDeleteClick={handleDeleteClick}
+          />
         </Pressable>
       )}
     />
